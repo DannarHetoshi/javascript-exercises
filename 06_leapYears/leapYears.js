e6eb4bfc17e90e2e72b8dea7eaeff69da63fcd8b
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-02 02:44:50 -0500

    modified:   06_leapYears/leapYears.js 	modified:   06_leapYears/leapYears.spec.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(theYear) {
 
-};
+    if (theYear % 4 === 0 && theYear % 400 === 0) {
+        return true
+    } else if (theYear % 100 === 0) {
+        return false
+    } else if (theYear % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+    // conditions
+    // 
+    // if divisible by 4 & 400 = leap year
+    // 
+    // else if divisible by 100 = not leap year
+    // 
+    // else if divisible by 4 = leap year
+    //
+    // else = not leap year
 
 // Do not edit below this line
 module.exports = leapYears;
