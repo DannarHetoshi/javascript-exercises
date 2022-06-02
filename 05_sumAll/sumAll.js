1d7261e2e810b5f818459cd2cbfdbeb3295f8481
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-02 02:27:45 -0500

    modified:   05_sumAll/sumAll.js 	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function(numOne, numTwo) {
+    let result = [];
+    let answer = 0;
+    if (numOne > 0 && typeof numOne === "number" && 
+            numTwo > 0 && typeof numTwo === "number") {
+        result = [numOne, numTwo].sort(function(a,b){return a-b});
+        answer = ((result[1]-result[0]+1)*(result[1]+result[0]))/2;
+    }
+    /*if (typeof numOne === "number" && 
+            typeof numTwo === "number") {
+        result = [numOne, numTwo].sort(function(a,b){return a-b});
+        answer = ((result[1]-result[0]+1)*(result[1]+result[0]))/2;
+    }*/ else {
+        answer = String("ERROR");    
+    }
+    console.log(answer);
+    return answer;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
