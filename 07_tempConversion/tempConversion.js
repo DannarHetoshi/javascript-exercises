b7632c8838e3686b919adcac7463cb2802d4b4a4
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-02 03:11:08 -0500

    modified:   07_tempConversion/tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
 const ftoc = function(newTemp) {
-  let newTempCelc = ((newTemp-32)*(5/9));
-  let ans = parseFloat(newTempCelc.toFixed(1))
+  //let newTempCelc = ((newTemp-32)*(5/9));
+  let ans = parseFloat(((newTemp-32)*(5/9)).toFixed(1))
   console.log(ans);
   return ans;
 };
 
 const ctof = function(newTemp) {
-  let newTempFar = (newTemp*(9/5)+32);
-  let ans = parseFloat(newTempFar.toFixed(1))
+  //let newTempFar = (newTemp*(9/5)+32);
+  let ans = parseFloat((newTemp*(9/5)+32).toFixed(1))
   console.log(ans);
   return ans;
 };
