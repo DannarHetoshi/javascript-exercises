f0ca3f6ffa0886004c346aaedf61084ea9ea01bc
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-01 23:06:00 -0500

    modified:   03_reverseString/reverseString.js 	modified:   03_reverseString/reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(result) {
+let answer = Array.from(result).reverse().join("");
+// let answer = answerReverse.reverse().join("");
+return answer;
 };
 
 // Do not edit below this line
