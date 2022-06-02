2a6970c2d83d9db3d67b08b2b3d81d0478788a5f
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-02 01:02:43 -0500

    modified:   04_removeFromArray/removeFromArray.js 	modified:   04_removeFromArray/removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(testArray, ...remValue) {
+    let arrLen = remValue.length;
+    let result = 0;
+    for (i = 0; i < arrLen; i++) {
+        console.log(remValue[i])
+        if (testArray.includes(remValue[i]) === false) {
+            continue;
+        }
+            result = testArray.indexOf(remValue[i]);
+            testArray.splice(result, 1);
+        }
+    return testArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
