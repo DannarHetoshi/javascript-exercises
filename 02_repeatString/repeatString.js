d5d41553db3f5df7939dab28c814ed62f8b78efa
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-01 22:26:37 -0500

    modified:   02_repeatString/repeatString.js 	modified:   02_repeatString/repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(testString, testNum) {
+    if (testNum < 0) return 'ERROR';
+    let string = '';
+    for (let i = 0; i < testNum; i++) {
+        string += testString;
+    }
+    return string;
 };
 
 // Do not edit below this line
