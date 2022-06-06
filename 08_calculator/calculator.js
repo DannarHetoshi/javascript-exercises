1e6b39a75708f15f8fd166eb8840a84c3c27b472
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-05 20:57:50 -0500

    modified:   08_calculator/calculator.js 	modified:   08_calculator/calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(a,b) {
+  return (a+b);
+};
+
+const subtract = function(a,b) {
+  return (a-b);
+};
+
+const sum = function(arr) {
+  const sum = arr.reduce((total, a) => total + a, 0);
+  return sum;
+};
+
+const multiply = function(arr) {
+  const product = arr.reduce((total, a) => total*a, 1)
+  return product;
+};
+
+const power = function(a,b) {
+  return (a**b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) { 
+  let fact = a
+  if (fact < 2) {
+    return 1;
+  }
+  for (i = a; i>1; i--) {
+    fact *= (i-1);
+  }
+  return fact;
 };
 
 // Do not edit below this line
