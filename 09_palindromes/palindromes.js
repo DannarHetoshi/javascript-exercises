30f6d285331caf5bc12ac9214c4f6f6a8a0bd270
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-05 21:32:28 -0500

    modified:   09_palindromes/palindromes.js 	modified:   09_palindromes/palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const answer = phrase.
+        toLowerCase()
+        .replace(/[^\w\s\']|_/g, "")
+        .replace(/\s+/g, "")
+        .split("");
+    
+    if (answer.join("") == answer.reverse().join("")) {
+        console.log(answer.join(""))
+        return true;
+    }
+    console.log(answer.join(""))
+    return false;
 };
 
 // Do not edit below this line
