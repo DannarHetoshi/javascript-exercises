fcc764d1f4a08f0c0e1646cff14b6eb118c90c59
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-05 22:17:26 -0500

    modified:   10_fibonacci/fibonacci.js 	modified:   10_fibonacci/fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(fib){
+    let n = parseInt(fib);
+    let arr = [0,1];
+    if (n < 0) return "OOPS";
+    if (n <= 2) return 1;
+    for (let i = 2; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+}
 // Do not edit below this line
 module.exports = fibonacci;
