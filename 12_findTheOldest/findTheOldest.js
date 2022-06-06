39c40014e27eee2d6ddff0f939364d284af7a919
DannarHetoshi <DannarHetoshi@gmail.com>
2022-06-06 00:00:51 -0500

    modified:   12_findTheOldest/findTheOldest.js 	modified:   12_findTheOldest/findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(age) {
+    const date = new Date();
+    for (let i = 0; i < age.length; i++)
+        if (age[i].yearOfDeath == undefined) {
+            age[i].yearOfDeath = date.getFullYear();
+        }
+    age.sort((a,b)=>
+                (a.yearOfDeath-a.yearOfBirth)>
+                (b.yearOfDeath-b.yearOfBirth) ? -1 : 1);
+    return age[0];
 };
 
 // Do not edit below this line
